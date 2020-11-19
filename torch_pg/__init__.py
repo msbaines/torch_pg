60b5fcf210b34dc60414e70a639297d28cb66bd1
Mandeep Singh Baines <mandeep.baines@gmail.com>
2020-11-18 18:18:11 -0800

    Don't throw an exception is the NCCL extension is not built

--- torch_pg/__init__.py
+++ torch_pg/__init__.py
@@ -1,8 +1,13 @@
+import logging
 import torch
 import torch_pg._C as capi
-import torch_pg._CUDA as cuda_api
 import torch.distributed.distributed_c10d as c10d
 
+try:
+    import torch_pg._CUDA as cuda_api
+except Exception:
+    cuda_api = None
+    pass
 
 def init_mpi():
     class ProcessGroupMPI:
@@ -13,5 +18,8 @@ def init_mpi():
     c10d._MPI_AVAILABLE = True
 
 def init_nccl():
+    if cuda_api is None:
+        logging.warn("cannot initialize nccl, extension not built")
+        return
     c10d.ProcessGroupNCCL = cuda_api.createProcessGroupNCCL
     c10d._NCCL_AVAILABLE = True
